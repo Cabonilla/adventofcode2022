9b93d1f3a2da3175e58657a32d3ac851b65abb4f
cabonilla <christian.bonilla01@gmail.com>
2022-12-06 15:32:39 -0800

    Fixed small inefficiency w/ pointer.

--- Day6/tuningtroub.py
+++ Day6/tuningtroub.py
@@ -7,35 +7,19 @@ head, tail = 0, 0
 id = ''
 id_len = 0
 
-id_four = ''
-id_four_pos = 0
-
 # Answer Part I
 
-# while tail < len(data):
-# 	if data[tail] not in id:
-# 		id += data[tail]
-# 		tail += 1
-# 		id_len = max(id_len, len(id))
-# 		if id_len == 4:
-# 			id_four = id
-# 			id_four_pos = head
-# 			break
-# 	else:
-# 		head += 1
-# 		maxLen = max(id_len, len(id))
-# 		id = id[1:]
-
-# Answer Part II
+id_four = ''
+id_four_pos = 0
 
 while tail < len(data):
 	if data[tail] not in id:
 		id += data[tail]
 		tail += 1
 		id_len = max(id_len, len(id))
-		if id_len == 14:
+		if id_len == 4:
 			id_four = id
-			id_four_pos = head
+			id_four_pos = tail
 			break
 	else:
 		head += 1
@@ -43,4 +27,26 @@ while tail < len(data):
 		id = id[1:]
 
 print(id_four)
-print(id_four_pos + 14)
+print(id_four_pos)
+
+# Answer Part II
+
+# id_fourteen = ''
+# id_fourteen_pos = 0
+
+# while tail < len(data):
+# 	if data[tail] not in id:
+# 		id += data[tail]
+# 		tail += 1
+# 		id_len = max(id_len, len(id))
+# 		if id_len == 14:
+# 			id_fourteen = id
+# 			id_fourteen_pos = tail
+# 			break
+# 	else:
+# 		head += 1
+# 		maxLen = max(id_len, len(id))
+# 		id = id[1:]
+
+# print(id_fourteen)
+# print(id_fourteen_pos)
